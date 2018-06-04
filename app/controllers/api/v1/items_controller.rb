8253fdcb83b86c096b2a08a810a15fb593f48a1f
heybluguy <heybluguy@yahoo.com>
2018-06-04 14:18:01 -0500

    Added item create.

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -3,4 +3,13 @@ class Api::V1::ItemsController < ApplicationController
     render json: {inventory: Item.all}
   end
 
+  def create
+    render json: Item.create(item_params)
+  end
+
+  private
+
+    def item_params
+      params.require(:item).permit(:name, :description, :unit_price)
+    end
 end
--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -10,15 +10,15 @@ describe "Items API" do
     expect(items.count).to eq(3)
   end
 
-  # it "create an item" do
-  #   item_params = {name: "Vibranium shield", description: "Captain America's tool of choice.", unit_price: 2}
+  it "create an item" do
+    item_params = {name: "Vibranium shield", description: "Captain America's tool of choice.", unit_price: 2}
 
-  #   post '/api/v1/items', params: {item: item_params}
+    post '/api/v1/items', params: {item: item_params}
 
-  #   expect(response).to be_success
+    expect(response).to be_success
 
-  #   item = Item.last
+    item = Item.last
 
-  #   expect(item.name).to eq(item_params[:name])
-  # end
+    expect(item.name).to eq(item_params[:name])
+  end
 end
