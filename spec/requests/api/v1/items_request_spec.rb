6b4d19f21ba13016c54bd2ac5bc29390baa8828a
heybluguy <heybluguy@yahoo.com>
2018-06-07 16:30:39 -0500

    Clean up

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -10,7 +10,7 @@ describe "Items API" do
     expect(items.count).to eq(3)
   end
 
-  it "create an item" do
+  it "Create an item" do
     item_params = {name: "Vibranium shield", description: "Round star spangled shield.", unit_price: 2}
 
     post '/api/v1/items', params: {item: item_params}
@@ -22,10 +22,10 @@ describe "Items API" do
     expect(item.name).to eq(item_params[:name])
   end
 
-  it "can update an item" do
+  it "Can update an item" do
     id = create(:item).id
     previous_name = Item.last.name
-    item_params = { name: "Sceptor" }
+    item_params = {name: "Sceptor" }
 
     put "/api/v1/items/#{id}", params: {item: item_params}
     item = Item.find_by(id: id)
